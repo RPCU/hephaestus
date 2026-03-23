5ec93a58201e8aa62396a6fcba961749e7ae67ba
Victor Hang <vhvictorhang@gmail.com>
2026-03-23 16:15:22 +0100

    chore 🧹 (kaas): upgrade kubernetes to v1.35.2
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/kaas/default.nix
+++ profiles/kaas/default.nix
@@ -105,8 +105,8 @@ in
     kubernetes = {
       enable = true;
       version = {
-        kubeadm = "v1.35.0";
-        kubelet = "v1.35.0";
+        kubeadm = "v1.35.2";
+        kubelet = "v1.35.2";
       };
     };
     caCertificates = {
